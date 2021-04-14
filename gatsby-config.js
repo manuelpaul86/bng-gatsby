37a02b0de5fae5d8ede337d6f868e96c1472422f
ManuP <38395410+manuelpaul86@users.noreply.github.com>
2021-04-14 17:06:05 +0200

    added gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,27 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/bng-icon.gif`,
+        cache_busting_mode: 'none',
+        theme_color_in_head: false,
+        },
+     },
+     {
+       resolve: 'gatsby-plugin-offline',
+       options: {
+         workboxConfig: {
+           globPatterns: ['src/assets/bng-icon.gif']
+         }
+       }
+     }
   ],
 };
